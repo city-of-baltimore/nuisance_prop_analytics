09013ec1699f1320d35439b48004b358c69e8631
Justin Elszasz <jtelszasz@gmail.com>
2021-08-01 21:30:59 -0400

    initial commit

---
+++ src/functions.py
@@ -0,0 +1,26 @@
+def connect_to_sql():
+    """Connects to BCIT SQL server
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
+    import os
+    import pyodbc
+    CITY_USERNAME = os.environ.get('CITY_USERNAME')
+    CITY_PWD = os.environ.get('CITY_PWD')
+    CITY_SQL_SERVER = os.environ.get('CITY_SQL_SERVER')
+    print(CITY_PWD)
+    conn = pyodbc.connect('Driver={FreeTDS Driver};'
+                          'Server=' + CITY_SQL_SERVER +
+                          ';Database=CitiStat'
+                          ';PWD=' + CITY_PWD +
+                          ';UID=BALTIMORE\\' + CITY_USERNAME +
+                          #';Trusted_Connection=yes'
+                          ';port=1433')
+
+    cursor = conn.cursor()
+    conn.close()
+    print("connection closed")
